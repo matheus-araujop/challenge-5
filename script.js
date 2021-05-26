5be6d18df71256464cc7fdf6d2ce339d588abedd
matheus-araujop <matheusp.araujo0@gmail.com>
2021-05-25 21:43:26 -0300

    Adicionado desafio 05

---
+++ script.js
@@ -0,0 +1,20 @@
+let data = [20,"Interestelar","Arabella","Sábado",true]
+console.log(data)
+// exibir array
+
+data.unshift("Matheus")
+console.log(data)
+// adiciona nome no início da lista
+
+data.pop()
+console.log(data)
+// remove o último da lista
+
+data.push("Vasco","Rio de Janeiro")
+console.log(data)
+//adiciona dois nomes ao fim da lista
+
+const numbers = [7,5,6,3,8,9,2,1,4]
+numbers.sort()
+console.log(numbers)
+// organizar em ordem crescente
